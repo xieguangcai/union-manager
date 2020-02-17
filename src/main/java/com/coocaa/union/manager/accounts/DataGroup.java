b3064655aed3b248a56680a4fd56ffee2294e62f
xieguangcai <xieguangcai@coocaa.com>
2020-02-17 21:53:33 +0800

    1。 新增权益配置。

---
+++ src/main/java/com/coocaa/union/manager/accounts/DataGroup.java
@@ -0,0 +1,114 @@
+package com.coocaa.union.manager.accounts;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author xieguangcai
+ * @date 2020/2/17
+ */
+@Entity
+@Table(name = "union_data_group", schema = "union_role", catalog = "")
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer","handler","fieldHandler"})
+public class DataGroup {
+    private int id;
+    private String key;
+    private String text;
+    private Date createTime;
+    private Date modifyTime;
+    private Integer sortId;
+
+    private Set<DataItems> items;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "key", nullable = false, length = 50)
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    @Basic
+    @Column(name = "text", nullable = false, length = 50)
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Basic
+    @Column(name = "create_time", nullable = true)
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Basic
+    @Column(name = "modify_time", nullable = true)
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    @Basic
+    @Column(name = "sort_id", nullable = true)
+    public Integer getSortId() {
+        return sortId;
+    }
+
+    public void setSortId(Integer sortId) {
+        this.sortId = sortId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DataGroup dataGroup = (DataGroup) o;
+        return id == dataGroup.id &&
+                Objects.equals(key, dataGroup.key) &&
+                Objects.equals(text, dataGroup.text) &&
+                Objects.equals(createTime, dataGroup.createTime) &&
+                Objects.equals(modifyTime, dataGroup.modifyTime) &&
+                Objects.equals(sortId, dataGroup.sortId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, key, text, createTime, modifyTime, sortId);
+    }
+
+    @OneToMany(targetEntity = DataItems.class, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "group_id")
+    public Set<DataItems> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<DataItems> items) {
+        this.items = items;
+    }
+}
---
+++ src/main/java/com/coocaa/union/manager/accounts/DataItems.java
@@ -0,0 +1,121 @@
+package com.coocaa.union.manager.accounts;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * @author xieguangcai
+ * @date 2020/2/17
+ */
+@Entity
+@Table(name = "union_data_items", schema = "union_role", catalog = "")
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer","handler","fieldHandler"})
+public class DataItems {
+    private int id;
+    private String value;
+    private String text;
+    private Integer sortId;
+    private Date createTime;
+    private Date modifyTime;
+
+    public DataItems() {
+    }
+
+    public DataItems(int dataItemId) {
+        this.id = dataItemId;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "value", nullable = false, length = 50)
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Basic
+    @Column(name = "text", nullable = false, length = 50)
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Basic
+    @Column(name = "sort_id", nullable = true)
+    public Integer getSortId() {
+        return sortId;
+    }
+
+    public void setSortId(Integer sortId) {
+        this.sortId = sortId;
+    }
+
+    @Basic
+    @Column(name = "create_time", nullable = true)
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Basic
+    @Column(name = "modify_time", nullable = true)
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DataItems dataItems = (DataItems) o;
+        return id == dataItems.id &&
+                Objects.equals(value, dataItems.value) &&
+                Objects.equals(text, dataItems.text) &&
+                Objects.equals(sortId, dataItems.sortId) &&
+                Objects.equals(createTime, dataItems.createTime) &&
+                Objects.equals(modifyTime, dataItems.modifyTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value, text, sortId, createTime, modifyTime);
+    }
+
+    private DataGroup dataGroup;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "group_id")
+    public DataGroup getDataGroup() {
+        return dataGroup;
+    }
+
+    public void setDataGroup(DataGroup dataGroup) {
+        this.dataGroup = dataGroup;
+    }
+}
